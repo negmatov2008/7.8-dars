131f250ae3020f9e9bc7d3ff14c68ad806ecba24
nodirdev984 <negmatovn984@gmail.com>
2024-02-24 20:58:23 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,14 @@ import { Provider, useSelector } from "react-redux";
 import store from "./redux/store";
 
 function App() {
- 
   return (
     <Provider store={store}>
-      <div className="max-w-[100%]  bg- relative  h-full bg-[url('./public/bg.png')] bg-no-repeat bg-cover ">
+      <div className="max-w-[100%]  bg- relative  h-full bg-[url('./public/bg.png')] bg-[linear-gradient(180deg, #1A043A 0%, #151278 70.31%, #2B1677 100%)] bg-no-repeat bg-cover ">
         <div className="max-w-[80%] pt-10 mx-auto">
           <Header />
           <Game />
         </div>
       </div>
-
-     
     </Provider>
   );
 }
